65c9a1926866d9803787b110ba685cebf034427c
tina3008 <kicja_3@ukr.net>
2024-05-24 18:16:20 +0300

    api

--- src/components/movie-api.js
+++ src/components/movie-api.js
@@ -1,48 +1,37 @@
-// const options = {
-//   method: "GET",
-//   headers: {
-//     accept: "application/json",
-//     Authorization:
-//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmY4NDRkMjBjYjAxMmE0MGU2OGJjNWViY2Q1M2I3NCIsInN1YiI6IjY2NGRmOGM0NTE5NTYwMmQ0YjQzY2U4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PX_uKoG-vAsOfEp68QuaSYaGGHH0Akieec3W_LMAUTU",
-//   },
-// };
+import axios from "axios";
+axios.defaults.baseURL = "https://api.themoviedb.org";
+export const getMovies = async () => {
+  // const options = {
+  //   method: "GET",
+    // headers: {
+      // accept: "application/json",
+      // Authorization:
+      //   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmY4NDRkMjBjYjAxMmE0MGU2OGJjNWViY2Q1M2I3NCIsInN1YiI6IjY2NGRmOGM0NTE5NTYwMmQ0YjQzY2U4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PX_uKoG-vAsOfEp68QuaSYaGGHH0Akieec3W_LMAUTU",
+      //    },
+  // };
+  // fetch("https://api.themoviedb.org/3/trending/movie/day", options)
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((response) => console.log(response))
+  //   .catch((err) => console.error(err));
 
-// fetch("https://api.themoviedb.org/3/trending/movie/day", options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
+// away===========================
 
-  import axios from "axios";
+const url =
+  "https://api.themoviedb.org/3/trending/movie/day";
 
-  export const getMovies = async () => {
-  const options = {
-    method: "GET",
+const options = {
     headers: {
-      accept: "application/json",
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmY4NDRkMjBjYjAxMmE0MGU2OGJjNWViY2Q1M2I3NCIsInN1YiI6IjY2NGRmOGM0NTE5NTYwMmQ0YjQzY2U4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PX_uKoG-vAsOfEp68QuaSYaGGHH0Akieec3W_LMAUTU",
-    },
-  };
+         },
+};
 
-  fetch("https://api.themoviedb.org/3/trending/movie/day", options)
-    .then((response) => {return response.json()})
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-
-    // try {
-    //   const response = await axios.get(
-    //     `https://api.themoviedb.org/3/trending/movie/day`,
-    //     {
-    //       params: {
-    //         accept: "application/json",
-    //         Authorization:
-    //           "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmY4NDRkMjBjYjAxMmE0MGU2OGJjNWViY2Q1M2I3NCIsInN1YiI6IjY2NGRmOGM0NTE5NTYwMmQ0YjQzY2U4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PX_uKoG-vAsOfEp68QuaSYaGGHH0Akieec3W_LMAUTU",
-    //       },
-    //     }
-    //   );
-    //   return response;
-    // } catch (error) {
-    //   console.error("Error fetching articles:", error);
-    //   throw error;
-    // }
-  };
+axios
+  .get(url, options)  
+  .then((response) => console.log(response.data))
+  .then((response) => {return response.data})
+  .catch((error) => console.log("Error fetching movie:", error));
+  
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+
+import {getMovies} from '../../components/movie-api'
 
 export default function HomePage() {
 const [movies, setMovies] = useState([]);
@@ -14,43 +15,9 @@ const [loading, setLoading] = useState(false);
     //  }
 
      async function fetchMovies() {
-       const options = {
-         method: "GET",
-         headers: {
-           accept: "application/json",
-           Authorization:
-             "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmY4NDRkMjBjYjAxMmE0MGU2OGJjNWViY2Q1M2I3NCIsInN1YiI6IjY2NGRmOGM0NTE5NTYwMmQ0YjQzY2U4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PX_uKoG-vAsOfEp68QuaSYaGGHH0Akieec3W_LMAUTU",
-         },
-       };
-
-      //  fetch("https://api.themoviedb.org/3/trending/movie/day", options)
-      //    .then((response) => {return response.json()})
-      //    .then((response) => console.log(response))
-      //    .catch((err) => console.error(err));
-  
-
-      //  try {
-         const response = await axios.get(
-           "<https://api.themoviedb.org/3/trending/movie/day>",        
-                {
-                  params: {
-              method: "GET",
-              headers: {
-                accept: "application/json",
-                Authorization:
-                  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmY4NDRkMjBjYjAxMmE0MGU2OGJjNWViY2Q1M2I3NCIsInN1YiI6IjY2NGRmOGM0NTE5NTYwMmQ0YjQzY2U4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PX_uKoG-vAsOfEp68QuaSYaGGHH0Akieec3W_LMAUTU",
-              },
-            },
-                },
-                
-         );
- console.log(response);
- return response;
-    
-      //  } catch (error) {
-      //    console.error("Error fetching articles:", error);
-      //    throw error;
-      //  }
+const qqq = await getMovies();
+        // console.log(qqq);
+      //  console.log(total_results);
      }
 
      fetchMovies();
